026ab3411dc647906a8b7a4a57821c1a05eba236
Rafael <rafael.anderka@gmail.com>
2018-11-06 00:43:08 +0000

    Remove levitation ski forces and fix some variable names

--- Main.m
+++ Main.m
@@ -14,78 +14,80 @@ clear; clc;
 % - Fix halbach array parameters (rewrite excel functions in matlab)
 % - Bisection for breaking distance calculation 
 % - Comment everything!
-% - Move results and plotting into seperate function
+% - Move three phases into single function that gets executed conditionally
+% - Remove levitation ski forces
 
-%% Import parameters
-halbach_wheel_parameters = importHalbachWheelParameters();
-halbach_array_parameters = importHalbachArrayParameters();
+%% Define parameters
+% Define basic parameters
+dt = 0.05;          % Time step (resolution)
+tmax = 80;          % Maximum allowed duration of run
+n_wheel = 6;        % Number of wheels
+distance_max = 450; % Experimentally found value for maximum distance of accelertaion phase
 
-% Define air gap
-g = 0.006;
-halbach_wheel_parameters.g = g;
+% Import parameters from .xlsx
+halbach_wheel_parameters = importHalbachWheelParameters(); % './Parameters/HalbachWheel_parameters.xlsx'
+halbach_array_parameters = importHalbachArrayParameters(); % './Parameters/HalbachArray_parameters.xlsx'
 
-dt = 0.05;
-tmax = 80;
+% These parameters should be moved into excel and their references need to
+% be replaced accordingly
+m_alpha = 111.755;
+rpm_max = 10000;
 
-angular_acceleration = 111.755; 
-rpm_max = 7000;
-distance_max = 800;
-n_wheel = 4;
+% Define max. possible angular velocity of Halbach wheel for specified max. rpm
+m_omega = rpm_max/60*2*pi;
 
-m_omega = rpm_max/60*2*pi; % 10000rpm
 
-%% Create time array and set intial conditions
-time = 0:dt:tmax;
-v = zeros(1,length(time));
-a = zeros(1,length(time));
-distance = zeros(1,length(time));
-omega = zeros(1,length(time));
-torque = zeros(1,length(time));
-f_drag_wheel = zeros(1,length(time)); % drag force from wheels
-f_lat_wheel = zeros(1,length(time)); %lateral force from halbach wheel
-f_drag_lev = zeros(1,length(time)); % drag force from skis
-f_lift_lev = zeros(1,length(time));
-f_drag_pod = zeros(1,length(time)); % force in x direction for the WHOLE POD
-f_lift_pod = zeros(1,length(time)); % force in y direction for the WHOLE POD
-power = zeros(1,length(time));
-efficiency = zeros(1,length(time)); % power output / power input
-slips = zeros(1,length(time));
-height = zeros(1,length(time));
+
+%% Initialize arrays
+%  Create all necessary arrays and initialize with 0s for each time step. 
+%  This is computationally faster than extending the arrays after each calculation.
+time = 0:dt:tmax;                       % Create time array with time step dt and maximum time tmax
+v = zeros(1,length(time));              % Velocity of pod
+a = zeros(1,length(time));              % Acceleration of pod
+distance = zeros(1,length(time));       % Distance travelled
+omega = zeros(1,length(time));          % Angular velocity of Halbach wheel
+torque = zeros(1,length(time));         % Torque on Halbach wheel
+power = zeros(1,length(time));          % Power
+efficiency = zeros(1,length(time));     % Power output / Power input
+slips = zeros(1,length(time));          % Slip between Halbach wheel and track
+f_thrust_wheel = zeros(1,length(time)); % Thrust force from wheels
+f_lat_wheel = zeros(1,length(time));    % Lateral force from halbach wheel    
+f_drag_pod = zeros(1,length(time));     % Drag force for whole pod
+f_lift_pod = zeros(1,length(time));     % Lift force for whole pod (UNUSED)
 
 %% Start for loop for acceleration phase 
 for i = 2:length(time)
     halbach_wheel_parameters.vt = v(i-1);
     
     % Find maximum slip and corresponding driving force by minimizing the negative of the driving force.
-    [slips(i), f_drag_wheel(i)] = fminbnd(@(x) -fx(x,v(i-1),halbach_wheel_parameters),0,50);
+    [slips(i), f_thrust_wheel(i)] = fminbnd(@(x) -fx(x,v(i-1),halbach_wheel_parameters),0,50);
+    
     omega(i) = (slips(i)+v(i-1))/halbach_wheel_parameters.ro; % Default case where omega isn't capped 
-    f_drag_wheel(i) = -halbach_wheel_parameters.w*f_drag_wheel(i)*n_wheel; % change sign so that force is positive
+    f_thrust_wheel(i) = -halbach_wheel_parameters.w*f_thrust_wheel(i)*n_wheel; % change sign so that force is positive
     f_lat_wheel(i) = halbach_wheel_parameters.w*fy(slips(i), v(i-1), halbach_wheel_parameters)*n_wheel; 
-
+    
     % Cap omega, if the angular acceleration is too huge (assume linear increase)
     omega_slope = abs(omega(i)-omega(i-1))/dt; % Calculate slope in change of omega
-    if omega_slope > m_omega 
-        omega(i) = omega(i-1) + m_omega*dt;
+    if omega_slope > m_alpha
+        omega(i) = omega(i-1) + m_alpha*dt;
         slips(i) = halbach_wheel_parameters.ro*omega(i) - v(i-1);
-        f_drag_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters); 
+        f_thrust_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters); 
     end
     halbach_wheel_parameters.s = slips(i);
     
-    % Calculate total drag force and lift force, which includes lev forces
-    [f_lift_lev(i),f_drag_lev(i), height(i)] = FDP_helper_fn_lev_forces(v(i-1), halbach_array_parameters); 
-    f_drag_pod(i) = f_drag_wheel(i)-f_drag_lev(i);
-    f_lift_pod(i) = f_lift_lev(i);
+    % Calculate total drag force
+    f_drag_pod(i) = f_thrust_wheel(i);
     
     % Calculate acceleration, velocity and distance
     a(i) = f_drag_pod(i)/halbach_wheel_parameters.M;
     v(i) = v(i-1) + dt*a(i);
-    distance(i) = distance(i-1)+dt*v(i); %forward euler (sohuld we use backward euler)
+    distance(i) = distance(i-1)+dt*v(i); %forward euler (should we use backward euler)
     
     % Torque, power and efficiency
-    torque(i) = halbach_wheel_parameters.ro*f_drag_wheel(i)/n_wheel;
-    power(i) = f_drag_wheel(i)*v(i); % power output = force * velocity
+    torque(i) = halbach_wheel_parameters.ro*f_thrust_wheel(i)/n_wheel;
+    power(i) = f_thrust_wheel(i)*v(i); % power output = force * velocity
     power_loss = halbach_wheel_parameters.w*n_wheel*pl(slips(i), v(i), halbach_wheel_parameters);
-    power_input = f_drag_wheel(i)*v(i)+power_loss; % ignoring inertia
+    power_input = f_thrust_wheel(i)*v(i)+power_loss; % ignoring inertia
     efficiency(i) = power(i)/power_input;
 
     k = i;
@@ -95,21 +97,19 @@ for i = 2:length(time)
     end
 end
 
-disp(i)
+%disp(i)
 
 %% For loop for maintaning rpm at our max rpm
 for i = k+1:length(time)
     omega(i) = omega(i-1);
     slips(i) = omega(i)*halbach_wheel_parameters.ro - v(i-1);
-    f_drag_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters);
+    f_thrust_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters);
     halbach_wheel_parameters.s = slips(i);
     f_lat_wheel(i) = halbach_wheel_parameters.w*fy(slips(i), v(i-1), halbach_wheel_parameters)*n_wheel; 
 
 
-    % Calculate total drag force and lift force, which includes lev forces
-    [f_lift_lev(i),f_drag_lev(i), height(i)] = FDP_helper_fn_lev_forces(v(i-1), halbach_array_parameters); 
-    f_drag_pod(i) = f_drag_wheel(i)-f_drag_lev(i);
-    f_lift_pod(i) = f_lift_lev(i);
+    % Calculate total drag force
+    f_drag_pod(i) = f_thrust_wheel(i);
     
     % Acceleration, velocity and distance
     a(i) = f_drag_pod(i)/halbach_wheel_parameters.M;
@@ -117,10 +117,10 @@ for i = k+1:length(time)
     distance(i) = distance(i-1)+dt*v(i);
     
     % Torque, power and efficiency
-    torque(i) = halbach_wheel_parameters.ro*f_drag_wheel(i)/n_wheel;
-    power(i) = f_drag_wheel(i)*v(i); % power output = force * velocity
+    torque(i) = halbach_wheel_parameters.ro*f_thrust_wheel(i)/n_wheel;
+    power(i) = f_thrust_wheel(i)*v(i); % power output = force * velocity
     power_loss = halbach_wheel_parameters.w*n_wheel*pl(slips(i), v(i), halbach_wheel_parameters);
-    power_input = f_drag_wheel(i)*v(i)+power_loss; % ignoring inertia
+    power_input = f_thrust_wheel(i)*v(i)+power_loss; % ignoring inertia
     efficiency(i) = power(i)/power_input;
 
     k = i;
@@ -130,35 +130,33 @@ for i = k+1:length(time)
     end
 end
     
-disp(distance(i));
+%disp(distance(i));
 
 %% Deceleration phase
 for i = k+1:length(time)
     halbach_wheel_parameters.vt = v(i-1);
     
     % Find maximum slip and corresponding driving force by minimizing the positive of the driving force.
-    [slips(i),f_drag_wheel(i)] = fminbnd(@(x) fx(x,v(i-1),halbach_wheel_parameters),-50,0);
+    [slips(i),f_thrust_wheel(i)] = fminbnd(@(x) fx(x,v(i-1),halbach_wheel_parameters),-50,0);
     
     % Default case where omega isn't capped 
-    f_drag_wheel(i) = halbach_wheel_parameters.w*f_drag_wheel(i)*n_wheel; % change sign so that force is positive
+    f_thrust_wheel(i) = halbach_wheel_parameters.w*f_thrust_wheel(i)*n_wheel;
     omega(i) = (slips(i)+v(i-1))/halbach_wheel_parameters.ro;
     f_lat_wheel(i) = halbach_wheel_parameters.w*fy(slips(i), v(i-1), halbach_wheel_parameters)*n_wheel; 
 
     
     % Cap omega, if the angular deceleration is too huge (assume linear increase)
     omega_slope = abs(omega(i)-omega(i-1))/dt; % Calculate slope in change of omega
-    if omega_slope > angular_acceleration 
-        omega(i) = omega(i-1) - angular_acceleration*dt;
+    if omega_slope > m_alpha 
+        omega(i) = omega(i-1) - m_alpha*dt;
         slips(i) = halbach_wheel_parameters.ro*omega(i) - v(i-1);
-        f_drag_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters); 
+        f_thrust_wheel(i) = halbach_wheel_parameters.w*n_wheel*fx(slips(i), v(i-1), halbach_wheel_parameters); 
     end
     
     halbach_wheel_parameters.s = slips(i);
     
-    % Calculate total drag force and lift force, which includes lev forces
-    [f_lift_lev(i),f_drag_lev(i), height(i)] = FDP_helper_fn_lev_forces(v(i-1), halbach_array_parameters); 
-    f_drag_pod(i) = f_drag_wheel(i)-f_drag_lev(i);
-    f_lift_pod(i) = f_lift_lev(i);
+    % Calculate total drag force
+    f_drag_pod(i) = f_thrust_wheel(i);
 
     % Acceleration, velocity and distance
     a(i) = f_drag_pod(i)/halbach_wheel_parameters.M;
@@ -166,10 +164,10 @@ for i = k+1:length(time)
     distance(i) = distance(i-1)+dt*v(i);
    
     % Torque, power and efficiency
-    torque(i) = halbach_wheel_parameters.ro*f_drag_wheel(i)/n_wheel;
-    power(i) = f_drag_wheel(i)*v(i); % power output = force * velocity
+    torque(i) = halbach_wheel_parameters.ro*f_thrust_wheel(i)/n_wheel;
+    power(i) = f_thrust_wheel(i)*v(i); % power output = force * velocity
     power_loss = halbach_wheel_parameters.w*n_wheel*pl(slips(i), v(i), halbach_wheel_parameters);
-    power_input = f_drag_wheel(i)*v(i)+power_loss; % ignoring inertia
+    power_input = f_thrust_wheel(i)*v(i)+power_loss; % ignoring inertia
     efficiency(i) = power(i)/power_input;
 
     k = i;
@@ -180,8 +178,8 @@ for i = k+1:length(time)
 end
 
 %% Truncate arrays and create struct array to store results 
-result = finalizeResults(k,time,distance,v,a,9.55*omega,torque,f_drag_wheel,f_drag_lev,f_lift_lev,...
-                         f_drag_pod,f_lift_pod,power,efficiency,slips,f_lat_wheel,height);
+result = finalizeResults(k, time, distance, v, a, omega * 60 / (2*pi), torque, f_thrust_wheel, f_lat_wheel,...
+                         f_drag_pod, f_lift_pod, power, efficiency, slips);
 
 %% Plot the trajectory graphs
 plotTrajectory(result.time,result.distance,result.velocity);
--- finalizeResults.m
+++ finalizeResults.m
@@ -1,4 +1,4 @@
-function [ result ] = finalizeResults(max,t,d,v,a,rpm,torque,f_drag_wheel,f_drag_lev,f_lift_lev,f_drag_pod,f_lift_pod,power,eff,slips,f_lat_wheel,height)
+function [ result ] = finalizeResults(max,t,d,v,a,rpm,torque,f_thrust_wheel,f_lat_wheel,f_drag_pod,f_lift_pod,power,eff,slips)
 % finalizeResults  Truncates trajectory arrays and creates a results structure
 % Inputs:
 %   max            Last used index in result arrays 
@@ -9,16 +9,14 @@ function [ result ] = finalizeResults(max,t,d,v,a,rpm,torque,f_drag_wheel,f_drag
 %   a              Acceleration over time array
 %   rpm            RPM over time array
 %   torque         torque over time array
-%   f_drag_wheel   Wheel drag force over time array
-%   f_drag_lev     Lev drag force over time array
-%   f_lift_lev     Lev lift force over time array
+%   f_thrust_wheel   Wheel drag force over time array
+%   f_lat_wheel    Lat wheel force over time array
 %   f_drag_pod     Pod drag force over time array
 %   f_lift_pod     Pod lift force over time array
 %   power          Power over time array
 %   eff            Efficiency over time array
 %   slips          Slips over time array
-%   f_lat_wheel    Lat wheel force over time array
-%   height         Height over time array
+
 % Output:
 %   [ result ]     Result structure
 % @author          Rafael Anderka
@@ -33,16 +31,13 @@ result.acceleration = a(1:max);
 result.distance = d(1:max);
 result.rpm = rpm(1:max);
 result.torque = torque(1:max);
-result.wheel_drag = f_drag_wheel(1:max);
-result.lev_drag = f_drag_lev(1:max);
-result.lev_lift = f_lift_lev(1:max);
+result.thrust = f_thrust_wheel(1:max);
 result.pod_drag = f_drag_pod(1:max);
 result.pod_lift = f_lift_pod(1:max);
 result.power = power(1:max);
 result.efficiency = eff(1:max);
 result.slips = slips(1:max);
-result.f_lat_wheel = f_lat_wheel(1:max);
-result.lev_height = height(1:max);
+result.wheel_lat = f_lat_wheel(1:max);
 
 end
 
