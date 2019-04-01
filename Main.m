7035204ae9dc4b3fc16282602bc119b639dcda79
Rafael <rafael.anderka@gmail.com>
2019-04-02 00:54:27 +0100

    Added support for stripes

--- Main.m
+++ Main.m
@@ -12,18 +12,21 @@
 
 clear; clc;
 
-%% Define parameters
-% Define basic parameters
-useMaxAccDistance = false;
+%% Parameters
+% Script mode
+useMaxAccDistance = false;          
 maxAccDistance = 1100;
-
-dt = 0.05;                      % Time step (see note above)
-tmax = 60;                      % Maximum allowed duration of run
-n_wheel = 6;                    % Number of wheels
-deceleration_total = 2.2*9.81;  % Braking deceleration from EmBrakes
 
 % Import parameters from './Parameters/HalbachWheel_parameters.xlsx'
 halbach_wheel_parameters = importHalbachWheelParameters();
+
+% Define basic parameters
+dt = 1/20;                          % Time step (see note above)
+tmax = 60;                          % Maximum allowed duration of run
+n_wheel = 6;                        % Number of wheels
+deceleration_total = 2.2 * 9.81;    % Braking deceleration from EmBrakes
+stripe_dist = 100 / 3.281;          % Distance between stripes
+number_of_stripes = floor(halbach_wheel_parameters.l / stripe_dist); % Total number of stripes we will detect
 
 %% Initialize arrays
 %  Create all necessary arrays and initialize with 0s for each time step. 
@@ -44,7 +47,7 @@ f_thrust_wheel = zeros(1,length(time)); % Thrust force from a single Halbach whe
 f_lat_wheel = zeros(1,length(time));    % Lateral force from a single Halbach wheel   
 f_x_pod = zeros(1,length(time));        % Net force in direction of track (x) for whole pod
 f_y_pod = zeros(1,length(time));        % Net force in lateral direction (y) for whole pod
-
+stripes = zeros(1,number_of_stripes);   % Indices at which we detect each stripe
 
 %% Calculation loop
 %  This is the main loop of the script, caluclating the relevant values for
@@ -55,13 +58,14 @@ f_y_pod = zeros(1,length(time));        % Net force in lateral direction (y) for
 %  phase = 2 -- Deceleration
 %  phase = 3 -- Max RPM
 
-phase = 1; % We start in the acceleration phase
+phase = 1;          % We start in the acceleration phase
+stripe_count = 0;   % Initially we have counted 0 stripes
 
 % For each point in time ...
 for i = 2:length(time) % Start at i = 2 because values are all init at 1
     %% Phase transitions
     % If we have exceeded the max. RPM we cap the RPM and recalculate
-    if (omega(i-1) * 60 / (2*pi)) > halbach_wheel_parameters.m_rpm
+    if (omega(i-1) * 60 / (2 * pi)) > halbach_wheel_parameters.m_rpm
         phase = 3; % Max RPM
         
         % Recalculate previous time = i - 1 to avoid briefly surpassing max RPM
@@ -90,13 +94,19 @@ for i = 2:length(time) % Start at i = 2 because values are all init at 1
     calc_main(phase, i, dt, n_wheel, v, a, distance, omega, torque, torque_lat, power, power_loss, power_input, efficiency, slips, ...
               f_thrust_wheel, f_lat_wheel, f_x_pod, f_y_pod, halbach_wheel_parameters, deceleration_total);
     
-    fprintf("%.2f ft, %.2f m, %.2f m/s, %.2f s\n", distance(i)*3.281, distance(i), v(i), time(i))
+    fprintf("%.2f ft, %.2f m, %.2f m/s, %.2f s\n", distance(i) * 3.281, distance(i), v(i), time(i))
+    
+    %% Stripes
+    if (distance(i) >= (1 + stripe_count) * stripe_dist)
+        stripes(1 + stripe_count) = i;
+        stripe_count = stripe_count + 1;
+    end
     
     %% Exit conditions
     % Stop when speed is 0m/s or time is up
     if v(i) <= 0 || i == length(time)
         % Truncate arrays and create final result structure 
-        result = finalizeResults(i, time, distance, v, a, omega * 60 / (2*pi), torque, torque_lat, f_thrust_wheel, f_lat_wheel,...
+        result = finalizeResults(i, time, distance, v, a, omega * 60 / (2 * pi), torque, torque_lat, f_thrust_wheel, f_lat_wheel,...
                                  f_x_pod, f_y_pod, power, power_loss, power_input, efficiency, slips);
         % Break from loop
         break;
